dfd646053e83335cda646c129448ce109c214ba7
Luis Galindo <egalindo54@uabc.edu.mx>
2021-04-13 19:57:11 -0600

    Metodo de falsa posicion agregado

--- raices.c
+++ raices.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <stdio.h>
 
-double eps()                    /* Epsilon, numero mas
+float eps()                    /* Epsilon, numero mas
                                  * pequeño que c puede manejar
                                  * que no sea 0*/
 {
@@ -15,7 +15,7 @@ double eps()                    /* Epsilon, numero mas
     return eps;
 }
 
-double Scarb(float n)           /* metodo de escarbeu */
+float Scarb(float n)           /* metodo de escarbeu */
 {
     int  n_int = 0;
     if(n <= 0)
@@ -28,10 +28,10 @@ double Scarb(float n)           /* metodo de escarbeu */
     return 0.5 * pow(10, n_int );
 }
 
-double ErrorA(float vac, float van) /* error aproximado
+float ErrorA(float vac, float van) /* error aproximado
                                      * (percentual) */
 {
-    double ea = fabs( vac - van );
+    float ea = fabs( vac - van );
     if( vac != 0.0 )
     {
         if( ea < eps() ) /* cualquier valor pequeño
@@ -46,9 +46,26 @@ double ErrorA(float vac, float van) /* error aproximado
     return ea;
 }
 
-float biseccion(double xi, double xu, double (*f) (double), int n )
+float primera_derivada(float x,float (*f) (float)) /* derivada */
 {
-    double
+    float
+        h = pow(2,-8);
+
+    float
+        fxph = f(x+h),
+        fxmh = f(x-h);
+
+    float
+        fp1 = (fxph - fxmh) / (2*h);
+
+    return fp1;
+}
+
+//float newton(float xi,float f)
+
+float biseccion(float xi, float xu, float (*f) (float)  )
+{
+    float
         xr = 0,                 /* x anterior */
         xrant = 0,              /* almacena xr para compararlo con
                                  * el nuevo valor */
@@ -94,22 +111,65 @@ float biseccion(double xi, double xu, double (*f) (double), int n )
     return xr;
 }
 
+float newton(float xi,  float (*f) (float)  )
+{
+    float
+        ea = 50,
+        es = Scarb(5),
+        x1 = 0;
 
+    while(ea > es)
+    {
+        x1 = xi - f(xi) / primera_derivada(xi,f);
+        ea = ErrorA(x1,xi);
+        xi = x1;
+    }
+    return xi;
+}
 
+float falsa_pos(float xi, float xu, float (*f) (float) )
+{
+    float
+        xr = 0,
+        xrant = 0,
+        ea = 50,
+        es = Scarb(5);
 
-double a(double b){
+    if ( ! ((f(xi) * f(xu)) > 0) )
+    {
+        while(ea > es)
+        {
+            xrant = xr;
+            xr = xu - (f(xu) * (xi - xu)) / (f(xi) - f(xu));
+            if( (f(xi) * f(xr)) < 0 )
+            {
+                xu = xr;
+            }
+            if( (f(xu) * f(xr)) < 0 )
+            {
+                xi = xr;
+            }
+            ea = ErrorA(xr, xrant);
+        }
+    }
+
+    return xr;
+}
+
+float a(float b){
     return (b*b)-5.0;
 }
 
 int main() {
-    printf("%f \n", biseccion(1, 3, a, 100) );
+    printf("%f \n", falsa_pos(1,3,a) );
     /* printf("%f \n", eps()); */
 
 
     /* for(int i = 0;i < 3;i++){ */
-    /*     for(int j = 0; j < 5; j++){ */
-    /*         printf(" i:%d j:%d - %f \n", i, j, ErrorA(i,j) ); */
-    /*     } */
+    /*     printf("%f", ); */
+    /*     /\* for(int j = 0; j < 5; j++){ *\/ */
+    /*      /\* printf(" i:%d - %f %f \n", i, a(i), primera_derivada(i,a) ); *\/ */
+    /*     /\* } *\/ */
     /* } */
 
     return 0;
